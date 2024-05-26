e32acc458cf0034ea6e6050321fe497a0a4558ce
SantiagoDavid312 <sdquishpe1@espe.edu.ec>
2024-05-25 23:29:09 -0500

    front

--- apps/SOAP-SERVER-CLIENT/BooksClient/BooksClient/Form1.Designer.cs
+++ apps/SOAP-SERVER-CLIENT/BooksClient/BooksClient/Form1.Designer.cs
@@ -29,23 +29,12 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.getAllLibrosResponseBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.buttonAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.getAllLibrosResponseBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(328, 203);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Cargar Datos";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // dataGridView1
             // 
@@ -78,7 +67,6 @@
             this.ClientSize = new System.Drawing.Size(990, 450);
             this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -90,7 +78,6 @@
 
         #endregion
         private System.Windows.Forms.BindingSource getAllLibrosResponseBindingSource;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonAdd;
     }
