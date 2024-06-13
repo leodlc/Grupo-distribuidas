c0b217976c23f7347d5bba98a2830db9d7a19e12
Leonardo <57681087+leodlc@users.noreply.github.com>
2024-06-12 19:46:15 -0500

    Home y CRUD género literario añadido

---
+++ apps/Libros_Proyecto/Presentacion_Web/Controllers/LiteraryGenreController.cs
@@ -0,0 +1,80 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Data;
+using Presentacion_Web.Service;
+
+namespace Presentacion_Web.Controllers
+{
+    public class LiteraryGenreController : Controller
+    {
+        private readonly LiteraryGenreService _literaryGenreService;
+
+        public LiteraryGenreController(LiteraryGenreService literaryGenreService)
+        {
+            _literaryGenreService = literaryGenreService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var genres = await _literaryGenreService.GetGenresAsync();
+            return View(genres);
+        }
+
+        public ActionResult Create()
+        {
+            return PartialView("_CrearGeneroForm", new GENEROLITERARIO());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(GENEROLITERARIO genre)
+        {
+            if (ModelState.IsValid)
+            {
+                await _literaryGenreService.CreateGenreAsync(genre);
+                return RedirectToAction("Index");
+            }
+            return PartialView("_CrearGeneroForm", genre);
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            var genre = await _literaryGenreService.GetGenreByIdAsync(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_CrearGeneroForm", genre);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(GENEROLITERARIO genre)
+        {
+            if (ModelState.IsValid)
+            {
+                await _literaryGenreService.UpdateGenreAsync(genre.IDGL, genre);
+                return RedirectToAction("Index");
+            }
+            return PartialView("_CrearGeneroForm", genre);
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var genre = await _literaryGenreService.GetGenreByIdAsync(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            await _literaryGenreService.DeleteGenreAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
