7f298a4ac4202878f7854a464629e2e4e30216e5
SantiagoDavid312 <sdquishpe1@espe.edu.ec>
2024-06-19 11:11:20 -0500

    cambio

--- apps/SOAP-SERVER-CLIENT/Ventas/Servicio/Controllers/CategoryController.cs
+++ apps/SOAP-SERVER-CLIENT/Ventas/Servicio/Controllers/CategoryController.cs
@@ -9,19 +9,30 @@ using Data;
 
 namespace Servicio.Controllers
 {
+    /// <summary>
+    /// Controlador para gestionar operaciones relacionadas con categorías.
+    /// </summary>
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     public class CategoryController : ApiController
     {
-        [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
-
+        /// <summary>
+        /// Envía una categoría para que se almacene en la base de datos.
+        /// </summary>
+        /// <param name="_categoria">La categoría a agregar.</param>
+        /// <returns>La nueva categoría ingresada.</returns>
         [HttpPost]
         public Categoria Add(Categoria _categoria)
         {
             var negocio = new Negocio.LCategoria();
             var categoria = negocio.Create(_categoria);
             return categoria;
-
         }
 
+        /// <summary>
+        /// Obtiene una categoría por su ID.
+        /// </summary>
+        /// <param name="id">El ID de la categoría a obtener.</param>
+        /// <returns>La categoría con el ID especificado.</returns>
         [HttpGet]
         public Categoria GetById(int id)
         {
@@ -30,15 +41,24 @@ namespace Servicio.Controllers
             return categoria;
         }
 
+        /// <summary>
+        /// Obtiene todas las categorías.
+        /// </summary>
+        /// <returns>Una lista de todas las categorías.</returns>
         [HttpGet]
         public List<Categoria> GetAll()
         {
             var negocio = new Negocio.LCategoria();
-
             var categoria = negocio.RetrieveAll().ToList();
             return categoria;
         }
 
+        /// <summary>
+        /// Actualiza una categoría existente.
+        /// </summary>
+        /// <param name="id">El ID de la categoría a actualizar.</param>
+        /// <param name="_categoria">Los datos actualizados de la categoría.</param>
+        /// <returns>La categoría actualizada.</returns>
         [HttpPut]
         public Categoria Update(int id, Categoria _categoria)
         {
@@ -47,16 +67,17 @@ namespace Servicio.Controllers
             return categoria;
         }
 
-
+        /// <summary>
+        /// Elimina una categoría por su ID.
+        /// </summary>
+        /// <param name="id">El ID de la categoría a eliminar.</param>
+        /// <returns>Verdadero si la categoría fue eliminada; de lo contrario, falso.</returns>
         [HttpDelete]
         public bool Delete(int id)
         {
-            //bool result=false;
             var negocio = new Negocio.LCategoria();
             var result = negocio.Delete(id);
             return result;
-
         }
-
     }
 }
